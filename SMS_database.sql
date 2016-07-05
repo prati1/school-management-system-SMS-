31fac23051cb8249b000ac3248c3ef84237b934a
Kiran995 <kir.prz@gmail.com>
2016-07-06 00:05:15 +0545

    Add files via upload

---
+++ SMS_database.sql
@@ -0,0 +1,35 @@
+create table if not exists standard(
+	standard_id int,
+	standard_name varchar(10),
+	PRIMARY KEY (standard_id),
+	student_id int references student(student_id)
+	);
+
+create table if not exists fee(
+	fee_pk int,
+	status varchar(5),
+	due_amount float(20),
+	type ENUM("tuition_fee", "bus_fee","hostel_fee")NOT NULL),
+	PRIMARY KEY(fee_pk)
+	); 
+
+create table if not exists tuition_fee(
+	monthly varchar(10),
+	fee_pk int,
+	PRIMARY KEY(fee_pk),
+	fee_pk int REFERENCES fee(fee_pk)
+);
+
+create table if not exists bus_fee(
+	yearly varchar(10),
+	fee_pk int,
+	PRIMARY KEY(fee_pk),
+	fee_pk int REFERENCES fee(fee_pk)
+);
+
+create table if not exists hostel_fee(
+	yearly varchar(10),
+	fee_pk int,
+	PRIMARY KEY(fee_pk),
+	fee_pk int REFERENCES fee(fee_pk)
+);
