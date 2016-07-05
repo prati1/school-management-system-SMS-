edd430e6b4d4c54e39d6cf5d5f693ab11e4d57a6
prati1 <meprati1@gmail.com>
2016-07-05 22:41:39 +0545

    Update left.php

--- left.php
+++ left.php
@@ -14,7 +14,18 @@
     float:left;
     padding:5px; 
 }
+# nav{list-style-type:none;}
+#nav_wrapper ul li a,visited{ color:#ccc;
+display:block;padding:15px;text-decoration:none;}
 
+
+#nav ul li a:hover{
+		color:#ccc;
+		text-decoration:none;
+	}
+#nav ul li:hover ul{display:block;}
+#nav ul ul {display: none;
+position:absolute;}
 }
 </style>
 </HEAD>
@@ -26,25 +37,51 @@
 
 <div id="nav">
 <a href="egg.php"target="main" >HOME </a><br><br>
+<a href="admin.php?class=1"target="main" >Mainviewpage</a><br><br>
+<!--
+<a href="viewbyclass.php?class=02"target="main" >Two </a><br><br>
+<a href="viewbyage.php?Age=21"target="main" >agetwentyone </a><br><br>
+-->
 <br>
-nursery<br>
-LKG<br>
-UKG<br>
-<a href="admin.php" target="main">one</a><br>
-two<br>
-three<br>
-four<br>
-five<br>
-six<br>
-seven<br>
-eight<br>
-nine<br>
-ten<br>
-eleven<br>
-twelve
-<br>
-
+<div id="nav_wrapper">
+<ul> <li>one </li>
+<ul>
+<li><a/= href="lo.php">one </a></li>
+<li><a/= href="lo.php">two</a></li>
+<li><a/= href="lo.php">three</a></li>
+</ul></ul>
 </div>
+<select><option value="null">------</option>
+<option value="lo.php">Nursery</option>
+  <option value="lo.php">LKG</option>
+  <option value="ukg">UKG</option>
+    <option value="lo.php">One</option>
+  <option value="two">Two</option>
+  <option value="three">Three</option>
+  <option value="four">Four</option>
+  <option value="five">Five</option>
+  <option value="six">Six</option>
+  <option value="seven">Seven</option>
+  <option value="eight">Eight</option>
+  <option value="ten">Ten</option>
+  <option value="nine">Nine</option>
+  <option value="eleven">Eleven</option>
+  <option value="twelve">Twelve</option></select>
+  
+  
+
+  
+<form action="viewbyage.php" method="post">
+	<label for='Age'>Age:</label><br>
+	<select name="Age">
+		<option value="NULl">------</option>
+		<option value="5">Five</option>
+		<option value="6">Six</option>
+		<option value="7">Seven</option>
+		<option value="21">Twenty One</option>
+	</select><br>
+	<input type="submit" name="val" value="Go" >
+</form>
 
 
 
