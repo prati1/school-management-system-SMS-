7a984a06dbc865fead8f126b22283062a9f39198
prati1 <meprati1@gmail.com>
2016-07-05 22:42:57 +0545

    Add files via upload

---
+++ viewbyage.php
@@ -0,0 +1,84 @@
+
+<html>
+<head>
+	<title>WELCOME TO ADMINISTRATION</title>
+	      
+		  	  <script type="text/javascript">
+         <!--
+            function getConfirmation(){
+               var retVal = confirm("Delete the student record?");
+               if( retVal == true ){
+                 //document.write ("User wants to continue!");
+				  //echo<a href="deletestudent.php?student_id=' . $row['student_id'] . '"></a>;
+				 $.get("deletestudent.php")
+				  //var x = new XMLHttpRequest();
+   // x.open("GET","deletestudent.php",true);
+    //x.send();
+                  return true;
+               }
+               else{
+                  document.write ("User does not want to continue!");
+                  return false;
+               }
+            }
+         //-->
+      </script>
+
+</head>
+<body>
+
+<?php
+/* 
+	VIEWBYAGE.PHP
+*/
+	 include('config.php');
+	 session_start();
+	 
+	$age = $_POST['Age'];
+	
+	 //$age=$_GET['age'];
+	// get results from database
+	$result = mysql_query("SELECT * FROM sms.administration where age=$age group by student_roll") 
+		or die(mysql_error());  
+	
+	// display data in table
+	echo "<p><b>View All</b>";
+	
+	echo "<table border='1' cellpadding='10'>";
+	echo "<tr> <th>ID</th><th>Roll No</th> <th>Name</th> <th>Gender</th><th>Address</th><th>Date of Admission</th><th>Date of Birth</th><th>Age</th><th>Guardian Name</th><th>Phone No</th><th>Email id </th> <th></th> <th></th></tr>";
+
+	// loop through results of database query, displaying them in the table
+	while($row = mysql_fetch_array( $result )) {
+			$id=$row['student_id'];
+		// echo out the contents of each row into a table
+		echo "<tr>";
+				//echo '<td>'<a href="adminfullview.php?student_id=' . $row['student_id'] . '"> . $row['student_id'] . </a>'</td>';
+		echo '<td><a href="adminfullview.php?student_id=' . $row['student_id'] . '"> $id </a></td>';
+		//echo '<td>' . $row['student_id'] . '</td>';
+		echo '<td>' . $row['student_roll'] . '</td>';
+		echo '<td>' . $row['Name'] . '</td>';
+		echo '<td>' . $row['gender'] . '</td>';
+		echo '<td>' . $row['temp_address'] . '</td>';
+		echo '<td>' . $row['date_of_admission'] . '</td>';
+		echo '<td>' . $row['DOBad'] . '</td>';
+		echo '<td>' . $row['age'] . '</td>';
+		echo '<td>' . $row['gName'] . '</td>';
+		echo '<td>' . $row['phone_no'] . '</td>';
+		echo '<td>' . $row['email_id'] . '</td>';
+		echo '<td><a href="edit.php?student_id=' . $row['student_id'] . '">Edit</a></td>';
+		echo '<td><a href="deletestudent.php?student_id=' . $row['student_id'] . '">Delete</a></td>';
+		//echo '<td><a href="#?student_id=' . $row['student_id'] . '" onclick="getConfirmation()">Delete</button></a></td>';
+		echo "</tr>"; 
+	} 
+
+	// close table>
+	echo "</table>";
+	  
+	  
+?>
+<p><a href="newreg.php">Add a new record</a></p>
+
+</body>
+</html>	
+	
+
---
+++ viewbyclass.php
@@ -0,0 +1,84 @@
+
+<html>
+<head>
+	<title>WELCOME TO ADMINISTRATION</title>
+	      
+		  	  <script type="text/javascript">
+         <!--
+            function getConfirmation(){
+               var retVal = confirm("Delete the student record?");
+               if( retVal == true ){
+                 //document.write ("User wants to continue!");
+				  //echo<a href="deletestudent.php?student_id=' . $row['student_id'] . '"></a>;
+				 $.get("deletestudent.php")
+				  //var x = new XMLHttpRequest();
+   // x.open("GET","deletestudent.php",true);
+    //x.send();
+                  return true;
+               }
+               else{
+                  document.write ("User does not want to continue!");
+                  return false;
+               }
+            }
+         //-->
+      </script>
+
+</head>
+<body>
+
+<?php
+/* 
+	VIEWBYCLASS.PHP
+	Displays all data from 'administration' table
+*/
+	 include('config.php');
+	 session_start();
+	 
+	 $standard=$_POST['standard_id'];
+	// get results from database
+	$result = mysql_query("SELECT * FROM sms.administration where standard_id=$standard group by student_roll") 
+		or die(mysql_error());  
+		
+		//We have not yet used standard_id in table administration. Once the standard table is filled, this attribute must be applied in administration
+	
+	// display data in table
+	echo "<p><b>View All</b>";
+	
+	echo "<table border='1' cellpadding='10'>";
+	echo "<tr> <th>ID</th><th>Roll No</th> <th>Name</th> <th>Gender</th><th>Address</th><th>Date of Admission</th><th>Date of Birth</th><th>Age</th><th>Guardian Name</th><th>Phone No</th><th>Email id </th> <th></th> <th></th></tr>";
+
+	// loop through results of database query, displaying them in the table
+	while($row = mysql_fetch_array( $result )) {
+		
+		// echo out the contents of each row into a table
+		echo "<tr>";
+				//echo '<td>'<a href="adminfullview.php?student_id=' . $row['student_id'] . '"> . $row['student_id'] . </a>'</td>';
+		echo '<td>' . $row['student_id'] . '</td>';
+		echo '<td>' . $row['student_roll'] . '</td>';
+		echo '<td>' . $row['Name'] . '</td>';
+		echo '<td>' . $row['gender'] . '</td>';
+		echo '<td>' . $row['temp_address'] . '</td>';
+		echo '<td>' . $row['date_of_admission'] . '</td>';
+		echo '<td>' . $row['DOBad'] . '</td>';
+		echo '<td>' . $row['age'] . '</td>';
+		echo '<td>' . $row['gName'] . '</td>';
+		echo '<td>' . $row['phone_no'] . '</td>';
+		echo '<td>' . $row['email_id'] . '</td>';
+		echo '<td><a href="edit.php?student_id=' . $row['student_id'] . '">Edit</a></td>';
+		echo '<td><a href="deletestudent.php?student_id=' . $row['student_id'] . '">Delete</a></td>';
+		//echo '<td><a href="#?student_id=' . $row['student_id'] . '" onclick="getConfirmation()">Delete</button></a></td>';
+		echo "</tr>"; 
+	} 
+
+	// close table>
+	echo "</table>";
+	  
+	  
+?>
+<p><a href="newreg.php">Add a new record</a></p>
+
+</body>
+</html>	
+	
+
